57c4d15d764de67ecb0dff9da844079504bd46f4
IT24100667 <IT24100667@my.sliit.lk>
2026-04-23 01:13:54 +0530

    fix: update CORS configuration to allow requests from the frontend URL

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const todoRoutes = require('./routes/todos');
 const app = express();
 const PORT = process.env.PORT
 
-app.use(cors());
+app.use(cors('https://todo-app-frontend-gray.vercel.app/'));
 app.use(express.json());
 
 app.use('/api/todos', todoRoutes);
